2a9f9f488f713dbe32c2e7b7646a9f374f409da5
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-02 13:43:13 +0200

    [UPD]  print for numbers divisible by 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // Stampa dei numeri da 1 a 100 utilizzando for
 for (let i = 1; i <= 100; i++) {
-  
+  // Se il numero è divisibile per 3 e per 5, stampa "FizzBuzz"
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} FizzBuzz`);
+  }
+  // Se il numero è divisibile solo per 3, stampa "Fizz"
+
+  // Se il numero è divisibile solo per 5, stampa "Buzz"
+
+  // Altrimenti, stampa il numero stesso
 }
